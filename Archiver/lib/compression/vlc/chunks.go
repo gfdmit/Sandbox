5e7e0b1a4fa23d9cc923722556c7a413e4f62383
Radmir <radmir00031@gmail.com>
2024-12-17 14:31:46 +0300

    archiver: full rewrite project

---
+++ Archiver/lib/compression/vlc/chunks.go
@@ -0,0 +1,88 @@
+package vlc
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type BinaryChunks []BinaryChunk
+
+type BinaryChunk string
+
+const chunksSize = 8
+
+func NewBinChunks(data []byte) BinaryChunks {
+	res := make(BinaryChunks, 0, len(data))
+
+	for _, code := range data {
+		res = append(res, NewBinChunk(code))
+	}
+
+	return res
+}
+
+func NewBinChunk(code byte) BinaryChunk {
+	return BinaryChunk(fmt.Sprintf("%08b", code))
+}
+
+func (bcs BinaryChunks) Bytes() []byte {
+	res := make([]byte, 0, len(bcs))
+
+	for _, bc := range bcs {
+		res = append(res, bc.Byte())
+	}
+
+	return res
+}
+
+func (bc BinaryChunk) Byte() byte {
+	num, err := strconv.ParseUint(string(bc), 2, chunksSize)
+	if err != nil {
+		panic("can't parse binary chunk: " + err.Error())
+	}
+
+	return byte(num)
+}
+
+func (bcs BinaryChunks) Join() string {
+	var buf strings.Builder
+
+	for _, bc := range bcs {
+		buf.WriteString(string(bc))
+	}
+
+	return buf.String()
+}
+
+func splitByChunks(bStr string, chunkSize int) BinaryChunks {
+	strLen := len(bStr)
+
+	chunksCount := strLen / chunkSize
+
+	if strLen/chunkSize != 0 {
+		chunksCount++
+	}
+
+	res := make(BinaryChunks, 0, chunksCount)
+
+	var buf strings.Builder
+
+	for i, ch := range bStr {
+		buf.WriteString(string(ch))
+
+		if (i+1)%chunkSize == 0 {
+			res = append(res, BinaryChunk(buf.String()))
+			buf.Reset()
+		}
+	}
+
+	if buf.Len() != 0 {
+		lastChunk := buf.String()
+		lastChunk += strings.Repeat("0", chunkSize-len(lastChunk))
+
+		res = append(res, BinaryChunk(lastChunk))
+	}
+
+	return res
+}
